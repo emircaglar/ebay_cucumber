b89dbd80e60a315e5af1740688f2779590c4a080
emircaglar <emircaglar2906@gmail.com>
2021-11-11 10:03:17 +0100

    sucht bei ebay mit DataBase suchen

--- src/test/java/stepDefinitions/_04_sucht_with_Databese.java
+++ src/test/java/stepDefinitions/_04_sucht_with_Databese.java
@@ -2,14 +2,19 @@ package stepDefinitions;
 
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Then;
+import pages.DialogContent;
+import pages.TopNav;
 import utilities.DataBaseUtility;
 
 import java.util.List;
 
 public class _04_sucht_with_Databese {
 
+    TopNav t=new TopNav();
     @Then("^Search a value und click with DataBase as query   \"([^\"]*)\"$")
     public void searchAValueUndClickWithDataBaseAsQuery(String query) {
         List<List<String>>dataBaseList= DataBaseUtility.getListData(query);
+        t.finElementandSendKeys("searchButton",dataBaseList.get(0).get(1));
+        t.finElementandClick("searchButtonClick");
     }
 }
