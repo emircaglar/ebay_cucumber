843c657ac9e8a0057cbb7f238ffca396b509aeeb
emircaglar <emircaglar2906@gmail.com>
2021-11-11 09:40:41 +0100

    sucht bei ebay mysql connection real

---
+++ src/test/java/featureFiles/_04_login_und_sucht_in_Database.feature
@@ -0,0 +1,4 @@
+Feature: Project with Database
+  Scenario: with database
+    Given Navigate to ebay
+    Then Search a value und click with DataBase
---
+++ src/test/java/stepDefinitions/_04_sucht_with_Databese.java
@@ -0,0 +1,9 @@
+package stepDefinitions;
+
+import cucumber.api.java.en.Then;
+
+public class _04_sucht_with_Databese {
+    @Then("^Search a value und click with DataBase$")
+    public void searchAValueUndClickWithDataBase() {
+    }
+}
